ab82519d179479bbd11b825940cc4e689ce7151f
VCassina <victorcassina@gmail.com>
2023-05-29 16:47:59 +0200

    !! CARROUSEL 90% patché, il manque l'absence de crash, version plus stable mais tjrs un crash sans valeur de index non modifiée - Tjrs via un effacement de byDateDesc.

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
@@ -7,66 +7,61 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+  const byDateDesc = data?.focus?.sort((evtA, evtB) =>
     new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
-  );
-  const [timeoutId, setTimeoutId] = useState(null);
+  ) || [];
+  const timeoutIdRef = useRef(null);
 
   const nextCard = () => {
     setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
   };
 
   useEffect(() => {
-    clearTimeout(timeoutId);
+    clearTimeout(timeoutIdRef.current);
 
-    const newTimeoutId = setTimeout(() => {
+    timeoutIdRef.current = setTimeout(() => {
       nextCard();
     }, 5000);
 
-    setTimeoutId(newTimeoutId);
-
     return () => {
-      clearTimeout(newTimeoutId);
+      clearTimeout(timeoutIdRef.current);
     };
   }, [index]);
 
   const handleRadioChange = (radioIdx) => {
     setIndex(radioIdx);
   };
+
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+      {byDateDesc.map((event, idx) => (
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-  <div className="SlideCard__pagination">
-     {byDateDesc.map((e, radioIdx) => (
-      <input
-        key={e.id}
-        type="radio"
-        name="radio-button"
-        checked={index === radioIdx}
-        onChange={() => handleRadioChange(radioIdx)}
-      />
-    ))} 
-  </div>
-</div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc.map((e, radioIdx) => (
+            <input
+              key={e.id}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onChange={() => handleRadioChange(radioIdx)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
